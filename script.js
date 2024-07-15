012acbf34dfc74864cb7212dcfa0aadd78e20050
Claudio9616 <clares9611@gmail.com>
2024-07-15 19:04:46 +0200

    aggiunta la logica delle classi dinamiche, da fissare il css statico

--- script.js
+++ script.js
@@ -2,31 +2,41 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
-console.log(circles)
+const name = document.getElementById('name');
+const nome = document.querySelector('.nome');
+const lastName = document.getElementById('last-name');
+const cognome = document.querySelector('.cognome');
+const age = document.getElementById('age');
+const età = document.querySelector('.età');
+const km = document.getElementById('km');
+const distanza = document.querySelector('.distanza');
+
+// PER QUANDO DEVO PRENDERE I VALORI DEGLI INPUT
+// const user = {
+//     nome: name.value.trim(),
+//     cognome: lastName.value.trim(),
+//     età: parseInt(age.value.trim()),
+//     distanza: parseInt(km.value.trim())
+// }
+
+
 let currentActive = 1;
-// QUI IMPONGO IO CHE PARTE DA 1, MA SULL'HTML MANTENGO LA CLASSE ACTIVE, 
-// SE SULL'HTML NON LA MANTENESSI ALLORA DEVO PARTIRE DA 0-----RIGA 18 HTML
 next.addEventListener('click', () => {
     currentActive++
-    // NON PUO' SUPERARE LA LUNGHEZZA DEL CIRCLE
     if (currentActive > circles.length) {
-        // SE VOLESSI FARE UN CAROSELLO SEMPLICE DEVO METTERE IL "-=" QUI SOTTO
         currentActive = circles.length;
     }
     update()
 })
 prev.addEventListener('click', () => {
     currentActive--
-    // NON PUO' SUPERARE LA LUNGHEZZA IMPOSTA A RIGA 5
     if (currentActive < 1) {
-        // SE DOVESSI FARE UN CAROSELLO SEMPLICE DEVO METTERE "CIRCLES.LENGTH" QUI SOTTO
         currentActive = 1;
     }
     update()
 })
 function update() {
     circles.forEach((circle, i) => {
-        // QUESTO PERCHE' IL CIRCLES COMPORTANDOSI COME UN ARRAY CONTA DA 0 VEDI IL CONSOLE.LOG(RIGA5)
         if (i < currentActive) {
             circle.classList.add('active');
         } else {
@@ -36,24 +46,64 @@ function update() {
     const actives = document.querySelectorAll('.active');
     progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
     if (currentActive === 1) {
-        prev.disabled = true
+        prev.disabled = true;
+        prev.classList.remove('text-white', 'bg-primary');
+        prev.classList.add('text-black', 'bg-secondary');
     } else if (currentActive === circles.length) {
-        next.disabled = true
+        next.disabled = true;
+        next.classList.add('text-black', 'bg-secondary');
+        next.classList.remove('text-white', 'bg-primary');
     } else {
-        next.disabled = false
-        prev.disabled = false
+        next.disabled = false;
+        prev.disabled = false;
+        next.classList.add('text-white', 'bg-primary');
+        next.classList.remove('text-black', 'bg-secondary');
+        prev.classList.add('text-white', 'bg-primary');
+        prev.classList.remove('text-black', 'bg-secondary');
     }
-    // fare uno switch, considera che qui non messo l'elemento text, ma devi far mostare un form alla volta, tipo nome e cognome, età, km, etc
-    // if (currentActive === 1) {
-    //     text.style.color = 'black'
-    //     text.style.display = 'block'
-    // } else if (currentActive === 2) {
-    //     text.style.color = 'red'
-    //     text.style.display = 'block'
-    // } else if (currentActive === 3) {
-    //     text.style.display = 'none'
-    // } else if (currentActive === 4) {
-    //     text.style.color = 'yellow'
-    //     text.style.display = 'block'
-    // }
+    const userClass = {
+        nome, cognome, età, distanza
+    }
+    switch (currentActive) {
+        case 1:
+            userClass.nome.classList.add('d-inline-block'),
+                userClass.nome.classList.remove('d-none'),
+                userClass.cognome.classList.remove('d-inline-block'),
+                userClass.cognome.classList.add('d-none'),
+                userClass.età.classList.remove('d-inline-block'),
+                userClass.età.classList.add('d-none'),
+                userClass.distanza.classList.remove('d-inline-block'),
+                userClass.distanza.classList.add('d-none')
+            break;
+        case 2:
+            userClass.nome.classList.remove('d-inline-block'),
+                userClass.nome.classList.add('d-none'),
+                userClass.cognome.classList.add('d-inline-block'),
+                userClass.cognome.classList.remove('d-none'),
+                userClass.età.classList.remove('d-inline-block'),
+                userClass.età.classList.add('d-none'),
+                userClass.distanza.classList.remove('d-inline-block'),
+                userClass.distanza.classList.add('d-none')
+            break;
+        case 3:
+            userClass.nome.classList.remove('d-inline-block'),
+                userClass.nome.classList.add('d-none'),
+                userClass.cognome.classList.remove('d-inline-block'),
+                userClass.cognome.classList.add('d-none'),
+                userClass.età.classList.add('d-inline-block'),
+                userClass.età.classList.remove('d-none'),
+                userClass.distanza.classList.remove('d-inline-block'),
+                userClass.distanza.classList.add('d-none')
+            break;
+        case 4:
+            userClass.nome.classList.remove('d-inline-block'),
+                userClass.nome.classList.add('d-none'),
+                userClass.cognome.classList.remove('d-inline-block'),
+                userClass.cognome.classList.add('d-none'),
+                userClass.età.classList.remove('d-inline-block'),
+                userClass.età.classList.add('d-none'),
+                userClass.distanza.classList.add('d-inline-block'),
+                userClass.distanza.classList.remove('d-none')
+            break;
+    }
 }
